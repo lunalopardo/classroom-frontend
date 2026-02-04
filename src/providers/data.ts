d88bf2366b81407ac6876e5924cb8a0bcad371c6
Luna Lopardo <luna.lopardo@gmail.com>
2026-02-04 14:31:24 -0300

    finalize create classes form

--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,5 +1,5 @@
 import {createDataProvider, CreateDataProviderOptions} from "@refinedev/rest";
-import {ListResponse} from "@/types";
+import {CreateResponse, ListResponse} from "@/types";
 import {BACKEND_BASE_URL} from "@/constants";
 import {HttpError} from "@refinedev/core";
 
@@ -64,6 +64,17 @@ const options: CreateDataProviderOptions = {
 
         },
     },
+
+    create: {
+        getEndpoint: ({resource}) => resource,
+
+        buildBodyParams: async({variables }) => variables,
+
+        mapResponse: async(response) => {
+            const json: CreateResponse = await response.json();
+            return json.data ?? [];
+        }
+    }
 };
 
 const {dataProvider} = createDataProvider(BACKEND_BASE_URL, options);
